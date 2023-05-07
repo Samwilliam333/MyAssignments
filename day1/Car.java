a5cd6255db5bb6d271de26fb05e76c9b1dc58637
Samwilliam333 <132688949+Samwilliam333@users.noreply.github.com>
2023-05-07 14:41:32 +0530

    Add files via upload
    Day1

---
+++ day1/Car.java
@@ -0,0 +1,54 @@
+package week1.day1;
+
+public class Car {
+	public void openTheDoor() {
+		System.out.println("openingTheDoor");
+	}
+	public void applyBreak() {
+		System.out.println("applyingbreak");
+	}
+	public void applyGear() {
+		System.out.println("applyingGear");
+	}
+	public void switchOnAc() {
+		System.out.println("switchOnAc");
+	}
+	public void applyAccelerate() {
+		System.out.println("applyAccelerate");
+	}
+	public void playMusic() {
+		System.out.println("playingMusic");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
